6df98b97a5270d2b169ee6e618b703f50004800f
so1ve <i@mk1.io>
2026-05-03 00:48:16 +0800

    copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -8,6 +8,24 @@ return {
     },
     keymap = {
       preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          local has_copilot, suggestion = pcall(require, "copilot.suggestion")
+
+          if has_copilot and suggestion.is_visible() then
+            suggestion.accept()
+            return true
+          end
+
+          if cmp.snippet_active() then
+            return cmp.accept()
+          end
+
+          return cmp.select_and_accept()
+        end,
+        "snippet_forward",
+        "fallback",
+      },
       ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-j>"] = { "select_next", "fallback_to_mappings" },
       ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,37 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function(_, opts)
+    require("copilot").setup(opts)
+
+    local group = vim.api.nvim_create_augroup("RayCopilotBlink", { clear = true })
+
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "BlinkCmpMenuOpen",
+      desc = "Hide Copilot inline suggestions while blink menu is open",
+      callback = function()
+        require("copilot.suggestion").dismiss()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "BlinkCmpMenuClose",
+      desc = "Restore Copilot inline suggestions after blink menu closes",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+  end,
+  opts = {
+    panel = {
+      enabled = false,
+    },
+    suggestion = {
+      auto_trigger = true,
+    },
+  },
+}
