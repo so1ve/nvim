4f3076c7dc5e6bb2d67bbb8110ef5bf9056557a8
Ray <i@mk1.io>
2026-05-03 18:50:33 +0800

    style: group editor options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,29 +1,36 @@
 local opt = vim.opt
 
+-- ui
 opt.termguicolors = true
-opt.mouse = "a"
-opt.mousemoveevent = true
-opt.clipboard = "unnamedplus"
-
 opt.number = true
 opt.signcolumn = "yes"
 opt.cursorline = true
-opt.scrolloff = 8
-opt.sidescrolloff = 8
 opt.laststatus = 3
 opt.cmdheight = 0
 opt.showmode = false
+
+-- interaction
+opt.mouse = "a"
+opt.mousemoveevent = true
+opt.clipboard = "unnamedplus"
 opt.confirm = true
 
+-- scrolling
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- wrapping
 opt.linebreak = true
 opt.breakindent = true
 
+-- indentation
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
 opt.expandtab = true
 opt.smartindent = true
 
+-- whitespace
 opt.list = true
 opt.listchars = {
   lead = "·",
@@ -32,19 +39,25 @@ opt.listchars = {
   nbsp = "␣",
 }
 
+-- search
 opt.ignorecase = true
 opt.smartcase = true
 opt.inccommand = "split"
 
+-- splits
 opt.splitright = true
 opt.splitbelow = true
 
+-- files
+opt.fileformat = "unix"
 opt.undofile = true
 opt.autoread = true
+
+-- responsiveness
 opt.updatetime = 250
 opt.timeoutlen = 400
 opt.completeopt = { "menu", "menuone", "noselect" }
 
-opt.fileformat = "unix"
+-- messages
 opt.shortmess:append("I")
 opt.messagesopt = "wait:1000,history:500,progress:c"
