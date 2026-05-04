abd170f7e9e6c9188dc6d1e7e8bfbe6774aa0061
so1ve <i@mk1.io>
2026-05-05 02:16:15 +0800

    update dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,7 +10,26 @@ return {
     notifier = {
       enabled = false,
     },
-    dashboard = {},
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config') })" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", padding = 1 },
+        { title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
     indent = {},
     statuscolumn = {},
     terminal = {},
