eb4466d0866880c3965a3568f3b0d078c0381221
so1ve <i@mk1.io>
2026-05-06 01:06:09 +0800

    colorful-menu

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -14,6 +14,7 @@ return {
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "nvim-mini/mini.nvim",
+    "xzbdmw/colorful-menu.nvim",
   },
   opts = {
     appearance = {
@@ -70,6 +71,21 @@ return {
       },
     },
     completion = {
+      menu = {
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 0,
