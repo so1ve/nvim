a4019ee60c3ba6341435192aa18ab52cb6981abd
so1ve <i@mk1.io>
2026-05-07 00:18:41 +0800

    remove trouble conflict keymappings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,9 +5,9 @@ return {
   keys = {
     { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics" },
     { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace diagnostics" },
-    { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols" },
-    { "<leader>xl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP definitions/references" },
-    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location list" },
-    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list" },
+    -- { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols" },
+    -- { "<leader>xl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP definitions/references" },
+    -- { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location list" },
+    -- { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list" },
   },
 }
