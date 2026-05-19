fb133fdf1e285c5e5f68b4ebc3276d944709bdd9
so1ve <i@mk1.io>
2026-05-19 15:45:10 +0800

    feat(multicursor): expose missing cursor management functions via keybindings

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -9,6 +9,13 @@ return {
     { "<leader>ms", mode = { "n", "x" }, desc = "Skip next match cursor" },
     { "<leader>mS", mode = { "n", "x" }, desc = "Skip previous match cursor" },
     { "<leader>mt", mode = { "n", "x" }, desc = "Toggle cursor" },
+    { "<leader>mA", mode = { "n", "x" }, desc = "Add all match cursors" },
+    { "<leader>m/", mode = "n", desc = "Add all search cursors" },
+    { "<leader>ma", mode = "n", desc = "Align cursors" },
+    { "<leader>mr", mode = "n", desc = "Restore cursors" },
+    { "<leader>mh", mode = { "n", "x" }, desc = "Previous cursor" },
+    { "<leader>ml", mode = { "n", "x" }, desc = "Next cursor" },
+    { "<leader>mx", mode = { "n", "x" }, desc = "Delete cursor" },
     { "<C-leftmouse>", mode = "n", desc = "Add cursor with mouse" },
     { "<C-leftdrag>", mode = "n", desc = "Drag cursor with mouse" },
     { "<C-leftrelease>", mode = "n", desc = "Release cursor with mouse" },
@@ -36,6 +43,10 @@ return {
       mc.matchSkipCursor(-1)
     end, { desc = "Skip previous match cursor" })
     vim.keymap.set({ "n", "x" }, "<leader>mt", mc.toggleCursor, { desc = "Toggle cursor" })
+    vim.keymap.set({ "n", "x" }, "<leader>mA", mc.matchAllAddCursors, { desc = "Add all match cursors" })
+    vim.keymap.set("n", "<leader>m/", mc.searchAllAddCursors, { desc = "Add all search cursors" })
+    vim.keymap.set("n", "<leader>ma", mc.alignCursors, { desc = "Align cursors" })
+    vim.keymap.set("n", "<leader>mr", mc.restoreCursors, { desc = "Restore cursors" })
     vim.keymap.set("n", "<C-leftmouse>", mc.handleMouse, { desc = "Add cursor with mouse" })
     vim.keymap.set("n", "<C-leftdrag>", mc.handleMouseDrag, { desc = "Drag cursor with mouse" })
     vim.keymap.set("n", "<C-leftrelease>", mc.handleMouseRelease, { desc = "Release cursor with mouse" })
@@ -50,8 +61,8 @@ return {
     end
 
     mc.addKeymapLayer(function(layer_map)
-      layer_map({ "n", "x" }, "<leader>mp", mc.prevCursor)
-      layer_map({ "n", "x" }, "<leader>mP", mc.nextCursor)
+      layer_map({ "n", "x" }, "<leader>mh", mc.prevCursor)
+      layer_map({ "n", "x" }, "<leader>ml", mc.nextCursor)
       layer_map({ "n", "x" }, "<leader>mx", mc.deleteCursor)
       layer_map("n", "A", append_at_line_end)
       layer_map("x", "I", mc.insertVisual)
