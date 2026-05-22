878bb6a43fda4ce6df2d7bcebdecf16e1bf201ff
so1ve <i@mk1.io>
2026-05-22 22:01:15 +0800

    snacks-fff

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,39 +1,44 @@
-local function find_files()
-  require("fff").find_files()
-end
-
-local function live_grep()
-  require("fff").live_grep()
-end
-
 return {
-  "dmtrKovalenko/fff.nvim",
-  cmd = {
-    "FFFFind",
-    "FFFScan",
-    "FFFRefreshGit",
-    "FFFClearCache",
-    "FFFHealth",
-    "FFFDebug",
-    "FFFOpenLog",
-  },
-  build = function()
-    require("fff.download").download_or_build_binary()
-  end,
-  keys = {
-    { "<leader><space>", find_files, desc = "Find files" },
-    { "<leader>ff", find_files, desc = "Find files" },
-    { "<leader>fg", live_grep, desc = "Live grep" },
-  },
-  opts = {
-    layout = {
-      prompt_position = "top",
+  {
+    "dmtrKovalenko/fff.nvim",
+    cmd = {
+      "FFFFind",
+      "FFFScan",
+      "FFFRefreshGit",
+      "FFFClearCache",
+      "FFFHealth",
+      "FFFDebug",
+      "FFFOpenLog",
     },
-    prompt_vim_mode = true,
-    keymaps = {
-      close = { "<Esc>", "q" },
-      move_down = { "<Down>", "<C-j>" },
-      move_up = { "<Up>", "<C-k>" },
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    opts = {},
+    config = function(_, opts)
+      require("fff").setup(opts)
+    end,
+  },
+  {
+    "so1ve/snacks-fff.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+      "dmtrKovalenko/fff.nvim",
+    },
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("snacks-fff").find_files()
+        end,
+        desc = "Find files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("snacks-fff").live_grep()
+        end,
+        desc = "Live grep",
+      },
     },
   },
 }
