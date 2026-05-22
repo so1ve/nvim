4771ac2d5f81b14db9110623df8b454f63f8b0ba
so1ve <i@mk1.io>
2026-05-23 03:05:53 +0800

    refactor: simplify fff plugin key mappings and remove unused functions and config function

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,11 +1,3 @@
-local function find_files()
-  require("snacks-fff").find_files()
-end
-
-local function live_grep()
-  require("snacks-fff").live_grep()
-end
-
 return {
   {
     "dmtrKovalenko/fff.nvim",
@@ -22,21 +14,28 @@ return {
       require("fff.download").download_or_build_binary()
     end,
     opts = {},
-    config = function(_, opts)
-      require("fff").setup(opts)
-    end,
   },
   {
-    dir = "D:/Workspace/snacks-fff.nvim",
-    name = "snacks-fff.nvim",
+    "so1ve/snacks-fff.nvim",
     dependencies = {
       "folke/snacks.nvim",
       "dmtrKovalenko/fff.nvim",
     },
     keys = {
-      { "<leader><space>", find_files, desc = "Find files" },
-      { "<leader>ff", find_files, desc = "Find files" },
-      { "<leader>fg", live_grep, desc = "Live grep" },
+      {
+        "<leader>ff",
+        function()
+          require("snacks-fff").find_files()
+        end,
+        desc = "Find files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("snacks-fff").live_grep()
+        end,
+        desc = "Live grep",
+      },
     },
   },
 }
