65cabf608d04bf2c27572421bdfb727083ac7269
so1ve <i@mk1.io>
2026-05-29 00:50:52 +0800

    update markdown ftplugni

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,44 +5,42 @@ vim.opt_local.conceallevel = 2
 vim.opt_local.formatoptions:remove("r")
 vim.opt_local.formatoptions:append("o")
 
-local function blockquote(line)
+local function blockquote_context(line)
   local indent = line:match("^%s*") or ""
-  local index = #indent + 1
-  local quote_count = 0
+  local cursor = #indent + 1
+  local depth = 0
 
-  while line:sub(index, index) == ">" do
-    quote_count = quote_count + 1
-    index = index + 1
-
-    while line:sub(index, index):match("%s") do
-      index = index + 1
-    end
+  while line:sub(cursor, cursor) == ">" do
+    depth = depth + 1
+    cursor = cursor + 1
+    cursor = line:find("%S", cursor) or (#line + 1)
   end
 
-  if quote_count == 0 then
+  if depth == 0 then
     return nil
   end
 
   return {
-    empty = line:sub(index):match("^%s*$") ~= nil,
-    prefix = indent .. string.rep("> ", quote_count),
+    empty = line:sub(cursor):match("^%s*$") ~= nil,
+    prefix = indent .. string.rep("> ", depth),
   }
 end
 
 vim.keymap.set("i", "<CR>", function()
-  local quote = blockquote(vim.api.nvim_get_current_line())
+  local quote = blockquote_context(vim.api.nvim_get_current_line())
 
   if not quote then
-    return "\r"
+    return "<CR>"
   end
 
   if quote.empty then
-    return "\21\r"
+    return "<C-U>"
   end
 
-  return "\r" .. quote.prefix
+  return "<C-G>u<CR>" .. quote.prefix
 end, {
   buffer = true,
   desc = "Continue or exit Markdown blockquote",
   expr = true,
+  replace_keycodes = true,
 })
