74648a2f167635da02cefc0119841992de6502fd
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-09 04:14:47 +0800

    feat: add snacks.nvim library to Lua language config

--- lua/config/languages/lua.lua
+++ lua/config/languages/lua.lua
@@ -14,6 +14,7 @@ return {
       opts = {
         library = {
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          { path = "snacks.nvim", words = { "Snacks" } },
           { path = "wezterm-types", mods = { "wezterm" } },
         },
       },
