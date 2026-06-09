60eaa0b930a5349cd9a23f84ea0e528cf2bea635
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-10 04:19:58 +0800

    feat(textobject-hud): enhance key hints and add builtin/mini_ai sources

--- lua/plugins/textobject-hud.lua
+++ lua/plugins/textobject-hud.lua
@@ -19,16 +19,81 @@ return {
   },
   opts = function()
     local hud = require("textobject-hud")
+    local key_hints = {
+      ["treesitter:@assignment.inner"] = "i=",
+      ["treesitter:@assignment.outer"] = "a=",
+      ["treesitter:@block.inner"] = "ib",
+      ["treesitter:@block.outer"] = "ab",
+      ["treesitter:@call.inner"] = "iF",
+      ["treesitter:@call.outer"] = "aF",
+      ["treesitter:@class.inner"] = "ic",
+      ["treesitter:@class.outer"] = "ac",
+      ["treesitter:@comment.inner"] = "i/",
+      ["treesitter:@comment.outer"] = "a/",
+      ["treesitter:@conditional.inner"] = "ii",
+      ["treesitter:@conditional.outer"] = "ai",
+      ["treesitter:@function.inner"] = "if",
+      ["treesitter:@function.outer"] = "af",
+      ["treesitter:@loop.inner"] = "il",
+      ["treesitter:@loop.outer"] = "al",
+      ["treesitter:@parameter.inner"] = "ia",
+      ["treesitter:@parameter.outer"] = "aa",
+      ["treesitter:@return.inner"] = "ir",
+      ["treesitter:@return.outer"] = "ar",
+      ["treesitter:@statement.outer"] = "as",
+    }
+    local builtin_ids = {
+      "w",
+      "W",
+      "s",
+      "p",
+      '"',
+      "'",
+      "`",
+      "(",
+      ")",
+      "b",
+      "[",
+      "]",
+      "{",
+      "}",
+      "B",
+      "<",
+      ">",
+      "t",
+    }
+    local mini_ai_ids = {
+      '"',
+      "'",
+      "`",
+      "(",
+      "[",
+      "{",
+      "<",
+      "a",
+      "b",
+      "f",
+      "q",
+      "t",
+    }
+
+    for _, id in ipairs(builtin_ids) do
+      key_hints["builtin:i" .. id] = "i" .. id
+      key_hints["builtin:a" .. id] = "a" .. id
+    end
+
+    for _, id in ipairs(mini_ai_ids) do
+      key_hints["mini_ai:i" .. id] = "i" .. id
+      key_hints["mini_ai:a" .. id] = "a" .. id
+    end
 
     return {
       sources = {
+        hud.sources.builtin,
         hud.sources.treesitter,
         hud.sources.mini_ai,
       },
-      key_hints = {
-        ["treesitter:@function.outer"] = { "]f", "[f", "]F", "[F" },
-        ["treesitter:@parameter.inner"] = { "]a", "[a", "]A", "[A" },
-      },
+      key_hints = key_hints,
     }
   end,
 }
