914260830feba83029ff93a4666a2fd456f47262
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-09 15:38:06 +0800

    feat(neogit): update diff and hunk header styles

--- lua/colors/undefined/integrations/neogit.lua
+++ lua/colors/undefined/integrations/neogit.lua
@@ -2,9 +2,21 @@ local M = {}
 
 function M.get(p)
   return {
-    NeogitHunkHeader = { fg = p.diff_header, bg = p.bg_alt, bold = true },
-    NeogitHunkHeaderHighlight = { fg = p.diff_header, bg = p.inactive_selection, bold = true },
-    NeogitHunkHeaderCursor = { fg = p.diff_header, bg = p.selection, bold = true },
+    NeogitDiffHeader = { fg = p.diff_header, bg = p.bg, bold = true },
+    NeogitDiffHeaderHighlight = { fg = p.diff_header, bg = p.bg, bold = true },
+    NeogitDiffHeaderCursor = { fg = p.diff_header, bg = p.bg, bold = true },
+    NeogitDiffContext = { bg = p.bg },
+    NeogitDiffContextHighlight = { bg = p.bg },
+    NeogitDiffContextCursor = { bg = p.bg },
+    NeogitDiffAdd = { fg = p.diff_add_fg, bg = p.diff_add_bg },
+    NeogitDiffAddHighlight = { fg = p.diff_add_fg, bg = p.diff_add_bg },
+    NeogitDiffAddCursor = { fg = p.diff_add_fg, bg = p.diff_add_bg },
+    NeogitDiffDelete = { fg = p.diff_delete_fg, bg = p.diff_delete_bg },
+    NeogitDiffDeleteHighlight = { fg = p.diff_delete_fg, bg = p.diff_delete_bg },
+    NeogitDiffDeleteCursor = { fg = p.diff_delete_fg, bg = p.diff_delete_bg },
+    NeogitHunkHeader = { fg = p.diff_header, bg = p.bg, bold = true },
+    NeogitHunkHeaderHighlight = { fg = p.diff_header, bg = p.bg, bold = true },
+    NeogitHunkHeaderCursor = { fg = p.diff_header, bg = p.bg, bold = true },
   }
 end
 
