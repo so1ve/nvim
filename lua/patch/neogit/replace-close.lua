299548ba79040584055dcfb4ed1c946b432b0826
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-12 13:46:13 +0800

    fix neogit

--- lua/patch/neogit/replace-close.lua
+++ lua/patch/neogit/replace-close.lua
@@ -1,29 +1,37 @@
 local hacks = require("utils.hacks")
 
 local M = {}
+local RESTORE_BUF = "neogit_replace_restore_buf"
 
 function M.patch()
   hacks.on_module("neogit.lib.buffer", function(Buffer)
-    hacks.wrap(Buffer, "neogit_replace_close_restores_window", "close", function(close)
+    hacks.wrap(Buffer, "neogit_replace_close_show", "show", function(show)
+      return function(buffer, ...)
+        if buffer.kind ~= "replace" then
+          return show(buffer, ...)
+        end
+
+        local win = vim.api.nvim_get_current_win()
+        local old = vim.w[win][RESTORE_BUF] or vim.api.nvim_get_current_buf()
+        local opened_win = show(buffer, ...)
+
+        buffer.old_buf = old
+        vim.w[opened_win][RESTORE_BUF] = old
+
+        return opened_win
+      end
+    end)
+
+    hacks.wrap(Buffer, "neogit_replace_close", "close", function(close)
       return function(buffer, ...)
         if buffer.kind == "replace" then
-          local win = buffer.win_handle
+          local win, old = buffer.win_handle, buffer.old_buf
+          local current = win and vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buffer.handle
 
-          if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_buf(win) == buffer.handle then
-            local old = buffer.old_buf
-            local buf = old and vim.api.nvim_buf_is_valid(old) and vim.api.nvim_buf_is_loaded(old) and old
-              or vim.api.nvim_create_buf(true, false)
-
-            vim.api.nvim_win_set_buf(win, buf)
-          end
-
-          if not vim.api.nvim_buf_is_valid(buffer.handle) then
-            if buffer.old_cwd then
-              vim.api.nvim_set_current_dir(buffer.old_cwd)
-              buffer.old_cwd = nil
-            end
-
-            return
+          if current and old and vim.api.nvim_buf_is_loaded(old) then
+            vim.bo[buffer.handle].bufhidden = "hide"
+            vim.api.nvim_win_set_buf(win, old)
+            vim.w[win][RESTORE_BUF] = nil
           end
         end
 
