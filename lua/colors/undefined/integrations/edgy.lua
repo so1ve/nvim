2c111f0a9e5edfd8e54214c24d8a1066ae104ae3
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-13 19:49:16 +0800

    upd edgy

--- lua/colors/undefined/integrations/edgy.lua
+++ lua/colors/undefined/integrations/edgy.lua
@@ -2,8 +2,7 @@ local M = {}
 
 function M.get(p)
   return {
-    EdgyTitleAccent = { fg = p.green, bg = p.bg },
-    EdgyTitle = { fg = p.fg_dim, bg = p.bg, bold = true },
+    EdgyTitle = { fg = p.green, bg = p.bg, bold = true },
     EdgyWinBar = { fg = p.subtle, bg = p.bg },
     EdgyWinBarNC = { fg = p.subtle, bg = p.bg },
   }
