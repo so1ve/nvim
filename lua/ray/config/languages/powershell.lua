c5b92bb92e02e740c5d131000cd14df076cd66a0
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-17 04:54:23 +0800

    feat: add PowerShell LSP server configuration with bundle path

--- lua/ray/config/languages/powershell.lua
+++ lua/ray/config/languages/powershell.lua
@@ -5,4 +5,9 @@ return {
       lsp = { "powershell_es" },
     },
   },
+  servers = {
+    powershell_es = {
+      bundle_path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "packages", "powershell-editor-services"),
+    },
+  },
 }
