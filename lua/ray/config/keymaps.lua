4074ffbe993b851561dd4412182729bb4e850cc0
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-16 18:09:18 +0800

    add line wrap toggle and reorder keymaps

--- lua/ray/config/keymaps.lua
+++ lua/ray/config/keymaps.lua
@@ -4,7 +4,9 @@ local window_resize = require("ray.utils.window-resize")
 
 local resize_step = 2
 
--- delete them since when lsp is not ready for references, `gr` triggers the builtin key hint menu instead of a warning indicating that no references are found
+-- Remove builtin mappings that conflict with plugin/LSP behavior. When LSP is not
+-- ready for references, `gr` otherwise opens the builtin key hint menu instead of
+-- showing the expected "no references" feedback.
 local conflict_keymaps = {
   n = { "gO", "gra", "gri", "grn", "grr", "grt", "grx", "<C-W>d", "<C-W><C-D>" },
   i = { "<C-S>" },
@@ -19,6 +21,7 @@ for mode, keys in pairs(conflict_keymaps) do
   end
 end
 
+-- Search
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 map("x", "/", function()
   return visual_search.search_keys("forward")
@@ -27,20 +30,27 @@ map("x", "?", function()
   return visual_search.search_keys("backward")
 end, { desc = "Search selected text backward", expr = true, replace_keycodes = true })
 
-map({ "c", "i" }, "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
+-- Editing
 map("n", "x", '"_x', { desc = "Delete without yanking" })
-
 -- Disable `q` since it is easy to hit by accident and enter recording mode, which can be confusing
 map("n", "q", "<Nop>", { noremap = true, silent = true })
+map("i", "<C-z>", "<C-o>u", { desc = "Undo" })
+map("i", "<C-y>", "<C-o><C-r>", { desc = "Redo" })
 
+-- Clipboard
+map({ "c", "i" }, "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
+map({ "n", "x" }, "<leader>yp", '"0p', { desc = "Paste yanked text" })
+map({ "n", "x" }, "<leader>yP", '"0P', { desc = "Paste yanked text before" })
+
+-- Leader basics
 -- Prevent bare <Leader> from falling back to Normal-mode <space>, which moves
 -- the cursor when no leader sequence is completed.
 map({ "n", "x" }, "<leader>", "<Nop>", { desc = "Leader", silent = true })
-map({ "n", "x" }, "<leader>yp", '"0p', { desc = "Paste yanked text" })
-map({ "n", "x" }, "<leader>yP", '"0P', { desc = "Paste yanked text before" })
 map("n", "<leader>w", "<cmd>write<CR>", { desc = "Write file" })
 map("n", "<leader>W", "<cmd>wall<CR>", { desc = "Write all files" })
+map("n", "<leader>lw", "<cmd>setlocal wrap!<CR>", { desc = "Toggle line wrap" })
 
+-- Quit / close
 map("n", "<leader>Q", "<cmd>qa<CR>", { desc = "Quit all" })
 map("n", "<leader>qb", function()
   Snacks.bufdelete()
@@ -49,6 +59,7 @@ map("n", "<leader>qt", "<cmd>tabclose<CR>", { desc = "Quit tab" })
 map("n", "<leader>qT", "<cmd>tabonly<CR>", { desc = "Quit other tabs" })
 map("n", "<leader>qw", "<C-W>c", { desc = "Delete window", remap = true })
 
+-- Tabs and lines
 map("n", "[t", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
 map("n", "]t", "<cmd>tabnext<CR>", { desc = "Next tab" })
 map("n", "[T", "<cmd>tabfirst<CR>", { desc = "First tab" })
@@ -57,6 +68,7 @@ map("n", "[<Space>", "O<Esc>", { desc = "Blank line above" })
 map("n", "]<Space>", "o<Esc>", { desc = "Blank line below" })
 map("n", "gK", "i<CR><Esc>", { desc = "Split line at cursor" })
 
+-- Windows
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
@@ -75,20 +87,20 @@ map("n", "<C-Down>", function()
 end, { desc = "Decrease window height" })
 map("n", "<leader>=", window_resize.equalize, { desc = "Equalize windows" })
 
+-- Insert-mode navigation
 map("i", "<A-h>", "<Left>", { desc = "Move left cursor" })
 map("i", "<A-j>", "<Down>", { desc = "Move down cursor" })
 map("i", "<A-k>", "<Up>", { desc = "Move up cursor" })
 map("i", "<A-l>", "<Right>", { desc = "Move right cursor" })
 
+-- Command-line navigation
 map("c", "<A-h>", "<Left>", { desc = "Move left in command line" })
 map("c", "<A-l>", "<Right>", { desc = "Move right in command line" })
 
 -- map("n", "go", [["0yi):!start <C-r>0<CR>]], { desc = "Open target with system app" })
 
+-- Diagnostics
 map("n", "<leader>cd", function()
   require("ray.config.diagnostics").open_float({ scope = "line" })
 end, { desc = "Line diagnostics" })
 map("n", "<leader>cq", vim.diagnostic.setloclist, { desc = "Diagnostics location list" })
-
-map("i", "<C-z>", "<C-o>u", { desc = "Undo" })
-map("i", "<C-y>", "<C-o><C-r>", { desc = "Redo" })
