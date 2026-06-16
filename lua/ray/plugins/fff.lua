df45806bf11fe170695d446007722018b92fac0d
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-16 19:21:26 +0800

    refactor: remove unused options from fff plugin config

--- lua/ray/plugins/fff.lua
+++ lua/ray/plugins/fff.lua
@@ -14,8 +14,6 @@ return {
       require("fff.download").download_or_build_binary()
     end,
     opts = {
-      prompt = " ",
-      title = "Files",
       prompt_vim_mode = true,
       wrap_around = true,
       layout = {
@@ -25,24 +23,15 @@ return {
           wrap = "bottom",
         },
         path_shorten_strategy = "middle_number",
-        anchor = "center",
       },
       keymaps = {
         close = { "<Esc>", "q" },
-        select = "<CR>",
-        select_split = "<C-s>",
-        select_vsplit = "<C-v>",
-        select_tab = "<C-t>",
         move_up = { "<Up>", "<C-p>", "<C-k>" },
         move_down = { "<Down>", "<C-n>", "<C-j>" },
         preview_scroll_up = "<C-b>",
         preview_scroll_down = "<C-f>",
         toggle_debug = "<A-d>",
         cycle_grep_modes = { "<S-Tab>", "<A-r>" },
-        cycle_previous_query = "<C-Up>",
-        cycle_forward_query = "<C-Down>",
-        toggle_select = "<Tab>",
-        send_to_quickfix = "<C-q>",
         focus_list = "/",
         focus_preview = "<A-p>",
       },
