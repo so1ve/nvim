272bee1056408d6d13345b29be131874278010ff
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-18 19:03:24 +0800

    fix

--- lua/ray/plugins/tiny-comment.lua
+++ lua/ray/plugins/tiny-comment.lua
@@ -1,4 +1,9 @@
 return {
   "so1ve/tiny-comment.nvim",
+  keys = {
+    { "gco", desc = "Add comment below" },
+    { "gcO", desc = "Add comment above" },
+    { "gcA", desc = "Add comment at end of line" },
+  },
   opts = {},
 }
