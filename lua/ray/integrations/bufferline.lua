f3e91608b5ec24e9becff5f7243293630c673261
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-19 21:52:53 +0800

    remove offset_spec

--- lua/ray/integrations/bufferline.lua
+++ lua/ray/integrations/bufferline.lua
@@ -1,27 +1,20 @@
 local M = {}
 
-function M.offset(filetype, text, opts)
-  return vim.tbl_extend("force", {
+function M.offset(filetype, text)
+  local offset = vim.tbl_extend("force", {
     filetype = filetype,
     text = text,
     text_align = "left",
     highlight = "BufferLineOffset",
     separator = true,
-  }, opts or {})
-end
+  })
 
-local function add_offset(opts, offset)
-  opts.options = opts.options or {}
-  opts.options.offsets = opts.options.offsets or {}
-  table.insert(opts.options.offsets, offset)
-end
-
-function M.offset_spec(offset)
   return {
     "akinsho/bufferline.nvim",
     optional = true,
     opts = function(_, opts)
-      add_offset(opts, offset)
+      opts.options.offsets = opts.options.offsets or {}
+      table.insert(opts.options.offsets, offset)
     end,
   }
 end
--- lua/ray/plugins/neotest.lua
+++ lua/ray/plugins/neotest.lua
@@ -195,6 +195,6 @@ return {
     },
   },
   edgy.view_spec("left", edgy.view("Neotest", "neotest-summary", { wo = { winbar = false } })),
-  bufferline.offset_spec(bufferline.offset("neotest-summary", "Neotest")),
+  bufferline.offset("neotest-summary", "Neotest"),
   edgy.view_spec("bottom", edgy.view("Neotest Output", "neotest-output-panel", { size = { height = 15 } })),
 }
