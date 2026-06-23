6dd83aff4c2587102a347057d1b609b7e64c13fd
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-24 04:09:59 +0800

    fmt

--- lua/ray/utils/keymaps.lua
+++ lua/ray/utils/keymaps.lua
@@ -14,13 +14,23 @@ function M.map_display_line_motion(buffer)
     silent = true,
   }
 
-  vim.keymap.set("n", "j", count_aware_motion("j", "gj"), vim.tbl_extend("force", base, {
-    desc = "Move down display line",
-  }))
+  vim.keymap.set(
+    "n",
+    "j",
+    count_aware_motion("j", "gj"),
+    vim.tbl_extend("force", base, {
+      desc = "Move down display line",
+    })
+  )
 
-  vim.keymap.set("n", "k", count_aware_motion("k", "gk"), vim.tbl_extend("force", base, {
-    desc = "Move up display line",
-  }))
+  vim.keymap.set(
+    "n",
+    "k",
+    count_aware_motion("k", "gk"),
+    vim.tbl_extend("force", base, {
+      desc = "Move up display line",
+    })
+  )
 end
 
 return M
