2dabf34eaa78963d114951fdd47aeca1888afe5e
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-25 00:29:51 +0800

    feat(icons): add custom icons for tsconfig and package.json files

--- lua/ray/plugins/mini-icons.lua
+++ lua/ray/plugins/mini-icons.lua
@@ -7,7 +7,27 @@ return {
   config = function()
     local icons = require("mini.icons")
 
-    icons.setup()
+    local tsconfig_icon = { glyph = "", hl = "MiniIconsAzure" }
+
+    icons.setup({
+      file = {
+        ["package.json"] = { glyph = "", hl = "MiniIconsRed" },
+        ["tsconfig.json"] = tsconfig_icon,
+      },
+    })
+
+    local get_icon = icons.get
+    icons.get = function(category, name)
+      if category == "file" and type(name) == "string" then
+        local basename = vim.fn.fnamemodify(name, ":t")
+        if basename:match("^tsconfig") then
+          return tsconfig_icon.glyph, tsconfig_icon.hl, false
+        end
+      end
+
+      return get_icon(category, name)
+    end
+
     icons.mock_nvim_web_devicons()
   end,
 }
