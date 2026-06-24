018d3e66e82fd88897a4dafe5c093672482d2a9f
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-24 16:15:18 +0800

    comment

--- lua/ray/plugins/treesj.lua
+++ lua/ray/plugins/treesj.lua
@@ -1,3 +1,19 @@
+-- not using mini.splitjoin because it doesn't support rust match arms
+--
+-- ```
+-- match arm {
+--     true => {
+--         1
+--     }
+-- }
+-- ```
+--
+-- to
+--
+-- ```
+-- match arm {
+--     true => 1
+-- }
 return {
   "Wansmer/treesj",
   keys = {
