8061ec3bc9600e1a22bd0d6f46e5cfe46db2844c
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-25 01:40:45 +0800

    upd

--- lua/ray/plugins/mini-icons.lua
+++ lua/ray/plugins/mini-icons.lua
@@ -3,31 +3,11 @@ return {
   virtual = true,
   dependencies = { "nvim-mini/mini.nvim" },
   lazy = false,
+  -- make sure it loads first
   priority = 1000,
   config = function()
     local icons = require("mini.icons")
-
-    local tsconfig_icon = { glyph = "", hl = "MiniIconsAzure" }
-
-    icons.setup({
-      file = {
-        ["package.json"] = { glyph = "", hl = "MiniIconsRed" },
-        ["tsconfig.json"] = tsconfig_icon,
-      },
-    })
-
-    local get_icon = icons.get
-    icons.get = function(category, name)
-      if category == "file" and type(name) == "string" then
-        local basename = vim.fn.fnamemodify(name, ":t")
-        if basename:match("^tsconfig") then
-          return tsconfig_icon.glyph, tsconfig_icon.hl, false
-        end
-      end
-
-      return get_icon(category, name)
-    end
-
+    icons.setup()
     icons.mock_nvim_web_devicons()
   end,
 }
