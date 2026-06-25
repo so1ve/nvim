3d0a63a2f50e4f8fad395db94b4b66ea930051d4
so1ve <58381667+so1ve@users.noreply.github.com>
2026-06-26 04:38:14 +0800

    clean

--- lua/ray/plugins/noice.lua
+++ lua/ray/plugins/noice.lua
@@ -58,16 +58,6 @@ return {
         {
           filter = {
             event = "notify",
-            cond = function(message)
-              return message.opts and message.opts.title == "Hardtime"
-            end,
-          },
-          view = "notify",
-          opts = { stop = true },
-        },
-        {
-          filter = {
-            event = "notify",
             error = true,
             find = "wakatime%-cli%.exe %-%-today",
           },
