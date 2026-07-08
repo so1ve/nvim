f67d503d48b0dbb3a3a0148f74e813123c308509
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-08 17:04:02 +0800

    remove treesitter-context

--- lua/ray/plugins/treesitter.lua
+++ lua/ray/plugins/treesitter.lua
@@ -47,25 +47,6 @@ return {
     end,
   },
   {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "so1ve/tiny-treesitter.nvim" },
-    opts = {
-      max_lines = 4,
-      mode = "topline",
-      multiline_threshold = 4,
-    },
-    keys = {
-      {
-        "gC",
-        function()
-          require("treesitter-context").go_to_context(vim.v.count1)
-        end,
-        desc = "Go to sticky context",
-      },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter-textobjects",
     branch = "main",
     opts = {
