c5df68cb499f35e9e3929dba78dbcf479743011c
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-15 15:19:21 +0800

    add virtual_lines for diagnostics

--- init.lua
+++ init.lua
@@ -306,6 +306,7 @@ vim.diagnostic.config({
   severity_sort = true,
   diagnostic_signs = { text = diagnostic_signs },
   virtual_text = true,
+  virtual_lines = { current_line = true },
 })
 
 -- #############################
